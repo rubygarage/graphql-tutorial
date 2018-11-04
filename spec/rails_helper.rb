7d86857d09bd0f8ef1550b354680e4ffc5ba811d
Svatok <s_s_s@ua.fm>
2018-11-04 18:15:35 +0200

    Setup project

---
+++ spec/rails_helper.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'rspec/rails'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
+
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
+
+RSpec::Matchers.define_negated_matcher :not_change, :change
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
